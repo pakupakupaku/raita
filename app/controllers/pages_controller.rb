5888ce1f53e2a731466ba76a9bc2c480e9b1f29d
adorechic <adorechic@gmail.com>
2016-06-05 13:39:45 +0900

    Add new action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def show
     @page = Page.find(params[:id])
   end
+
+  def new
+    @page = Page.new
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe PagesController, type: :controller do
       expect(assigns(:page)).to eq page
     end
   end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+      expect(assigns(:page)).to be_a_new(Page)
+    end
+  end
 end
