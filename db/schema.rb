68a7940390c0aa2b3f1baaf2470c71afe2f271a4
adorechic <adorechic@gmail.com>
2016-06-05 12:14:11 +0900

    Add page model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529102521) do
+ActiveRecord::Schema.define(version: 20160605031216) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
