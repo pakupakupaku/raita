1dc74ef5885d024bc4af9e1cc0e6bac0d3eec384
adorechic <adorechic@gmail.com>
2016-06-05 12:30:56 +0900

    Ignore unnecessary generates

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,11 @@ module Raita
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.skip_routes true
+      g.assets      false
+      g.helper      false
+      g.view_specs  false
+    end
   end
 end
